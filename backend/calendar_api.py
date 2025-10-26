aef0a3cfe4d8817f9ded237d5b5b1a7b506a2fb5
kanzoujapan <kanjisakai.kanjisakai@gmail.com>
2025-10-26 19:15:42 +0900

    update

--- backend/calendar_api.py
+++ backend/calendar_api.py
@@ -9,6 +9,8 @@ load_dotenv()
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 
+# from front to GET /api/auth 
+# redirect to Endpoint to initiate Google Oauth2 authentication
 @api_bp.get("/auth")
 def auth():
     base_url = "https://accounts.google.com/o/oauth2/v2/auth"
@@ -27,6 +29,42 @@ def auth():
     auth_url = f"{base_url}?{urllib.parse.urlencode(params)}"
     return redirect(auth_url)
 
+
+@api_bp.get("/oauth2callback")
+def authentication_callback():
+    token_url = "https://oauth2.googleapis.com/token"
+    code = request.args.get("code")
+    state = request.args.get("state")
+    client_id = os.getenv("GOOGLE_CLIENT_ID")
+    client_secret = os.getenv("GOOGLE_CLIENT_SECRET")
+    redirect_uri = os.getenv("GOOGLE_REDIRECT_URI")
+
+    sendData = {
+        "code": code,
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "redirect_uri": redirect_uri,
+        "state": state,
+        "grant_type": "authorization_code"
+    }
+
+    response = requests.post(token_url, data = sendData)
+    token_response_data = response.json()
+    access_token = token_response_data.get("access_token")
+
+    
+
+    
+
+    
+
+
+
+
+
+# from front to POST /api/plan
+# receive date and return it with currrent timestamp in JST
+# 受け取った日付をgoogle calendar api に問い合わせる、そしてその日のイベントを取得する
 @api_bp.post("/plan")
 def plan():
     data = request.get_json(silent=True) or {}
@@ -37,3 +75,5 @@ def plan():
     jst = zoneinfo.ZoneInfo("Asia/Tokyo")
     ts  = datetime.now(jst).strftime("%H:%M")
     return jsonify({"date": date, "timestamp": ts})
+
+
